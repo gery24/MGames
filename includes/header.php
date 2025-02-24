de5772e1a79271c2830dd95b5e4b37cbf540735d
Mure <miguelmure@ceroca.cat>
2025-02-24 20:12:38 +0100

    Nuevos css para estar mas organizados

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titulo ?? 'MGames - Tu tienda de videojuegos'; ?></title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/product.css">
+    <link rel="stylesheet" href="css/segunda_mano.css">
+    <link rel="stylesheet" href="css/admin.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
